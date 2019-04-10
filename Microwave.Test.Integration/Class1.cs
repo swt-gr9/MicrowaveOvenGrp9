90dc6a98c9a2cd682fa83c98b81ba17f0065f6fb
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-04-10 11:53:25 +0200

    Udskiftet integration nunit test med class library

---
+++ Microwave.Test.Integration/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true,Is.True);
+        }
+    }
+}
