bc9bc50717c0c8d5f956c4a98974a2b173497f58
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-04-23 14:51:07 +0200

    Test 7 done

--- Microwave.Test.Integration/IT7_CookControllerPowertube.cs
+++ Microwave.Test.Integration/IT7_CookControllerPowertube.cs
@@ -7,6 +7,7 @@ using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
+using NSubstitute.Routing.Handlers;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
@@ -32,24 +33,26 @@ namespace Microwave.Test.Integration
             _display = new Display(_output);
             _powerTube = new PowerTube(_output);
             _userInterface = Substitute.For<IUserInterface>();
-            _uut = new CookController(_timer, _display, _powerTube);
+            _uut = new CookController(_timer, _display, _powerTube, _userInterface);
         }
-
-        #region States
-
-        private void SetStateCooking()
-        {
-        }
-
-        #endregion
-
-
+        
         #region MainScenario
 
-
-        public void TestTurnsOn()
+        [TestCase(100)]
+        public void TestTurnsOn(int power)
         {
+            _uut.StartCooking(power, 120);
+            
+            _output.Received(1).OutputLine($"PowerTube works with {power} %");
+        }
 
+        [Test]
+        public void TestTurnOff()
+        {
+            _uut.StartCooking(50, 120);
+            
+            _timer.Expired += Raise.EventWith<EventArgs>();
+            _output.Received(1).OutputLine($"PowerTube turned off");
         }
 
         #endregion
