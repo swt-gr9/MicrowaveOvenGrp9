daec72e8d9234492d1948ae8036de648b2778139
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-04-23 13:38:15 +0200

    Test 7 oprettet

---
+++ Microwave.Test.Integration/IT7_CookControllerPowertube.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    public class IT7_CookControllerPowertube
+    {
+        #region Properties
+
+        private IUserInterface _userInterface;
+        private ICookController _uut;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IOutput _output;
+
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _timer = Substitute.For<ITimer>();
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+            _userInterface = Substitute.For<IUserInterface>();
+            _uut = new CookController(_timer, _display, _powerTube);
+        }
+
+        #region States
+
+        private void SetStateCooking()
+        {
+        }
+
+        #endregion
+
+
+        #region MainScenario
+
+
+        public void TestTurnsOn()
+        {
+
+        }
+
+        #endregion
+    }
+}
