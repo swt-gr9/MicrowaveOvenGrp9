3106f58b8dc24e0f6c113620649d6bab8a2742e0
Morten Rosenquist <201706031@post.au.dk>
2019-04-24 14:27:40 +0200

    Ændret test 9

--- Microwave.Test.Integration/IT9_TimerCookController.cs
+++ Microwave.Test.Integration/IT9_TimerCookController.cs
@@ -16,31 +16,62 @@ namespace Microwave.Test.Integration
         #region properties
 
         private IDisplay _display;
-        private ICookController _cookController;
+        private CookController _uut;
         private IPowerTube _powerTube;
-        private ITimer _uut;
+        private ITimer _timer;
         private IOutput _output;
         private IUserInterface _userInterface;
+        private ILight _light;
+        private IDoor _door;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+
         #endregion
 
         [SetUp]
         public void SetUp()
         {
             _output = Substitute.For<IOutput>();
-            _userInterface = Substitute.For<IUserInterface>();
+
+
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+
+            _door = new Door();
+
             _powerTube = new PowerTube(_output);
             _display = new Display(_output);
-            _uut = new Timer();
-            _cookController = new CookController(_uut,_display,_powerTube,_userInterface);
+            _light = new Light(_output);
+
+            _timer = new Timer();
+
+            _uut = new CookController(_timer, _display, _powerTube);
+
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _uut);
+
+            _uut.UI = _userInterface;
         }
 
         #region Main Scenario
 
-        [TestCase(60, "Display shows: 00:59",2000)]
-        [TestCase(120,"Display shows: 01:57",5000)]
-        public void OnTimerTickCookControllerLogsOutput(int time, string output, int delay)
+        [TestCase(1, "Display shows: 00:59",2000)]
+        [TestCase(2,"Display shows: 01:57",5000)]
+        public void OnTimerTickCookControllerLogsOutput(int times, string output, int delay)
         {
-            _cookController.StartCooking(50, time);
+            _door.Open();
+            _door.Close();
+
+            _powerButton.Press();
+            for (int i = times; (i > 0); i--)
+            {
+                _timeButton.Press();
+            }
+           
+            _startCancelButton.Press();
+
+            
             System.Threading.Thread.Sleep(delay);
             _output.Received(1).OutputLine(output);            
         }
